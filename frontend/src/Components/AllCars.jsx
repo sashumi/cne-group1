259c418d38313d744298f45703d1bc815250661d
MP <example>
2021-04-22 14:22:20 +0100

    Changed cars and Garage

--- frontend/src/Components/AllCars.jsx
+++ frontend/src/Components/AllCars.jsx
@@ -8,24 +8,12 @@ const Cars = () => {
             <strong>Select a car:</strong>
             <ul>
                 <li>
-                    <Link to="/cars/readbyid/1">Car 1</Link>
-                    <Link to="/cars/readbyname/carexample1">Car Example 1</Link>
-                </li>
-                <li>
-                    <Link to="/cars/readbyid/2">Car 2</Link>
-                    <Link to="/cars/readbyname/carexample2">Car Example 2</Link>
-                </li>
-                <li>
-                    <Link to="/cars/readbyid/3">Car 3</Link>
-                    <Link to="/cars/readbyname/carexample3">Car Example 3</Link>
-                </li>
-                <li>
-                    <Link to="/cars/readbyid/4">Car 4</Link>
-                    <Link to="/cars/readbyname/carexample4">Car Example 4</Link>
-                </li>
-                <li>
-                    <Link to="/cars/readbyid/5">Car 5</Link>
-                    <Link to="/cars/readbyname/carexample5">Car Example 5</Link>
+                    ID: <Link to="/cars/readbyid/1">Car 1</Link>
+                    Colour:
+                    Doors:                    
+                    Make:
+                    Model:
+                    Name: <Link to="/cars/readbyname/carexample1">Car Example 1</Link>
                 </li>
             </ul>
             <p>{id}</p>
--- frontend/src/Components/AllGarages.jsx
+++ frontend/src/Components/AllGarages.jsx
@@ -9,18 +9,7 @@ const Garages = () => {
             <ul>
                 <li>
                     <Link to="/garage/readbyid/1">Garage 1</Link>
-                </li>
-                <li>
-                    <Link to="/garage/readbyid/2">Garage 2</Link>
-                </li>
-                <li>
-                    <Link to="/garage/readbyid/3">Garage 3</Link>
-                </li>
-                <li>
-                    <Link to="/garage/readbyid/4">Garage 4</Link>
-                </li>
-                <li>
-                    <Link to="/garage/readbyid/5">Garage 5</Link>
+                    Name:
                 </li>
             </ul>
             <p>{id}</p>
